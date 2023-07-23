34919a48e1c2ced8f78ca9502ab6a6362f1259c9
kakon-khairulislam <kakon190299@gmail.com>
2023-07-23 07:11:52 +0600

    first commit

---
+++ webapicrud/Controllers/CrudMvcController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using webapicrud.Models;
+
+namespace webapicrud.Controllers
+{
+    public class CrudMvcController : Controller
+    {
+        // GET: CrudMvc
+        HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            List<Employee> emp_list = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44331/api/Crudapi");
+           var response = client.GetAsync("Crudapi");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                emp_list = display.Result;
+            }
+            return View(emp_list);
+        }
+    }
+}
---
+++ webapicrud/Controllers/CrudapiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapicrud.Models;
+
+namespace webapicrud.Controllers
+{
+    public class CrudapiController : ApiController
+    {
+        web_api_crud_dbEntities db = new web_api_crud_dbEntities();
+        [HttpGet]
+        public IHttpActionResult GetEmployees()
+        {
+            List<Employee> list = db.Employees.ToList();
+
+            return Ok(list);
+        }
+    }
+}
